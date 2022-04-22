ad820dea8d578e44209a96af4dddd9a7323962ff
mdzam <mdzam@ZMR>
2022-04-22 15:27:58 +0530

    Connection poling

--- jobLobby/src/main/java/com/codezmr/connection/DbConnection.java
+++ jobLobby/src/main/java/com/codezmr/connection/DbConnection.java
@@ -3,23 +3,30 @@ package com.codezmr.connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
 public class DbConnection {
-	
-	/* Use Connection Pooling
-	 * Use Properties file
-	 * Use static factory design pattern for connection 
-	 * */
-	
+
 	static Connection con;
 	
 	public static Connection getConnect() { 
 		
 		try {
 			
-			Class.forName("com.mysql.jdbc.Driver");
-
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/joblobby", "root", "codezmr");			
-
+			/*
+			 * Class.forName("com.mysql.jdbc.Driver");
+			 * 
+			 * con = DriverManager.getConnection("jdbc:mysql://localhost:3306/joblobby",
+			 * "root", "codezmr");
+			 */
+			
+			MysqlDataSource ds = new MysqlDataSource();
+			ds.setURL("jdbc:mysql://localhost:3306/joblobby");
+			ds.setUser("root");
+			ds.setPassword("codezmr");
+			
+			con = ds.getConnection();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
